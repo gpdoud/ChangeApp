65fd2e3fbf7845392f8641118afa79c8a3b67950
Greg Doud <gpdoud@gmail.com>
2021-07-06 09:29:37 -0400

    Add project files.

---
+++ ChangeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChangeApp {
+    class Program {
+        static void Main(string[] args) {
+            var cents = EnterCents();
+            var origCents = cents;
+            var quarters = cents / 25;
+            cents -= quarters * 25;
+            var dimes = cents / 10;
+            cents -= dimes * 10;
+            var nickels = cents / 5;
+            cents -= nickels * 5;
+            var pennies = cents;
+            Console.WriteLine($"{origCents} cents is {quarters} quarters, {dimes} dimes, {nickels} nickels, and {pennies} pennies.");
+        }
+        static int EnterCents() {
+            Console.Write("Enter the number of cents: ");
+            var centsStr = Console.ReadLine();
+            var cents = Int32.Parse(centsStr);
+            return cents;
+        }
+    }
+}
